688a252391925982f0f8661cbbed0573fd14023e
pdh0128 <pdh0128a@gmail.com>
2025-11-20 08:14:50 +0900

    delete :: memorial vectorizing service 삭제

--- app/feed/service/__init__.py
+++ app/feed/service/__init__.py
@@ -2,12 +2,10 @@
 Feed service module.
 """
 
-from .memorial_vectorizing_service import MemorialVectorizingService
 from .memorial_vector_store_service import MemorialVectorStoreService
 from .memorial_vector_delete_service import MemorialVectorDeleteService
 
 __all__ = [
-    "MemorialVectorizingService",
-    "MemorialVectorStoreService", 
+    "MemorialVectorStoreService",
     "MemorialVectorDeleteService"
 ]
